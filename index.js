ebb821be6bd68d194c7fa5ca3f59eb8cc29596e4
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-01-22 00:19:31 +0530

    index modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();  //will listen to incoming req. and sends out responses
 
 app.get('/', (req, res) => {  //
-  res.send({ bye: 'there' });  //
+  res.send({ hi: 'there' });  //
 });                           //
 
 const PORT = process.env.PORT || 5000; //can inject environment varibles. Allows heroko/railway to send runtime port to use. 5000 for development, others for production
