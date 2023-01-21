09975033f95cb1b92e199174c567ce0f702faa44
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-01-21 23:05:40 +0530

    Added server JSON

---
+++ server/index.js
@@ -0,0 +1,11 @@
+//import express from 'express';
+
+const express = require('express');
+const app = express();  //will listen to incoming req. and sends out responses
+
+app.get('/', (req, res) => {  //
+  res.send({ hi: 'there' });  //
+});                           //
+
+const PORT = process.env.PORT || 5000; //can inject environment varibles. Allows heroko/railway to send runtime port to use. 5000 for development, others for production
+app.listen(PORT);
