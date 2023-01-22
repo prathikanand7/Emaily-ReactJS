1a88e63688cbe5eaeaf748fb14ed5ece9a02c30d
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-01-22 15:49:28 +0530

    added new handler

--- index.js
+++ index.js
@@ -33,5 +33,7 @@ app.get(
   })
 );
 
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 const PORT = process.env.PORT || 5000; //can inject environment varibles. Allows heroko/railway to send runtime port to use. 5000 for development, others for production
 app.listen(PORT);
