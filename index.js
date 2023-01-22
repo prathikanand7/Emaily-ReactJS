38e002fb2b2f79b7be4aa701d0ac8b159fdcebdf
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-01-22 14:44:23 +0530

    added route handler

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 //import express from 'express';
 
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20')
-.Strategy;
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys"); // no need to add .js if its a js file
 
-const app = express();  //will listen to incoming req. and sends out responses
+const app = express(); //will listen to incoming req. and sends out responses
 
-//needto go to console.cloud.google.com to create OAuth strategy
-//Client ID : 312607634307-9ibju3dk67pl0fn4tiqjcfc1ok6ap6dm.apps.googleusercontent.com
-//Client secret: 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
+//need to go to console.cloud.google.com to create OAuth strategy
 
-passport.use(new GoogleStrategy()); //creates a new instance of  g strategy
+//Client ID - is a public token used to identify our servers to anyone in the world
+//client secrect should not be shared - as it will let anyone on globe to have unprecedented acess to our project
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+); //creates a new instance of google strategy for OAuth
+
+//Routehandler
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
 const PORT = process.env.PORT || 5000; //can inject environment varibles. Allows heroko/railway to send runtime port to use. 5000 for development, others for production
 app.listen(PORT);
