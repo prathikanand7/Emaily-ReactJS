997fbf2cc9821c3f9ec5f64a54a31aa417079b7e
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-02-10 16:25:12 +0530

    ..

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ require("./routes/authRoutes")(app);
 //need to go to console.cloud.google.com to create OAuth strategy
 
 //Client ID - is a public token used to identify our servers to anyone in the world
-//client secrect should not be shared - as it will let anyone on globe to have unprecedented acess to our project.
+//client secrect should not be shared - as it will let anyone on globe to have unprecedented acess to our project..
 
 const PORT = process.env.PORT || 5000; //can inject environment varibles. Allows heroko/railway to send runtime port to use. 5000 for development, others for production
 app.listen(PORT);
