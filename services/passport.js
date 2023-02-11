913d0277397c658da690460443975fb7af07226c
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-02-11 21:40:34 +0530

    await and async implementation.

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,7 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys"); // no need to add .js if its a js file
 const mongoose = require("mongoose");
+const { use } = require("passport");
 const User = mongoose.model("user");
 
 //managing cookies
@@ -26,18 +27,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((exisingUser) => {
-        if (exisingUser) {
-          //we already have a record with given profileID
-          done(null, exisingUser);
-        } else {
-          //we dont have this user record and so need to make a new user
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (exisingUser) {
+        //we already have a record with given profileID
+        return done(null, exisingUser);
+      }
+      //we dont have this user record and so need to make a new user
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 ); //creates a new instance of google strategy for OAuth
